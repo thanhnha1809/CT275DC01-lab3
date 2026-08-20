430a9d6e0b44582235f40e5bc9065e896005f629
Huynh Thanh Nha <huynhthanhnhagt123123@gmail.com>
2026-08-20 19:02:01 +0700

    Update add.php

--- public/add.php
+++ public/add.php
@@ -1,13 +1,31 @@
 <?php
 require_once __DIR__ . '/../src/bootstrap.php';
 
+use CT275\Labs\Contact;
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $contactData = [
+        'name' => $_POST['name'] ?? '',
+        'phone' => $_POST['phone'] ?? '',
+        'notes' => $_POST['notes'] ?? '',
+    ];
+
+    $contact = new Contact($PDO);
+    $errors = $contact->validate($contactData);
+    if (empty($errors)) {
+        $contact->fill($contactData);
+        $contact->save() && redirect('/');
+    }
+}
+
 include_once __DIR__ . '/../src/partials/header.php';
 ?>
 
 <body>
   <?php include_once __DIR__ . '/../src/partials/navbar.php' ?>
 
-  <!-- Main Page Content -->
   <div class="container">
 
     <?php
@@ -20,7 +38,6 @@ include_once __DIR__ . '/../src/partials/header.php';
 
         <form method="post" class="col-md-6 offset-md-3">
 
-          <!-- Name -->
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" name="name" class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>" maxlen="255" id="name" placeholder="Enter Name" value="<?= isset($_POST['name']) ? html_escape($_POST['name']) : '' ?>" />
@@ -32,7 +49,6 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
-          <!-- Phone -->
           <div class="mb-3">
             <label for="phone" class="form-label">Phone Number</label>
             <input type="text" name="phone" class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>" maxlen="255" id="phone" placeholder="Enter Phone" value="<?= isset($_POST['phone']) ? html_escape($_POST['phone']) : '' ?>" />
@@ -44,7 +60,6 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
-          <!-- Notes -->
           <div class="mb-3">
             <label for="notes" class="form-label">Notes </label>
             <textarea name="notes" id="notes" class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>" placeholder="Enter notes (maximum character limit: 255)"><?= isset($_POST['notes']) ? html_escape($_POST['notes']) : '' ?></textarea>
@@ -56,7 +71,6 @@ include_once __DIR__ . '/../src/partials/header.php';
             <?php endif ?>
           </div>
 
-          <!-- Submit -->
           <button type="submit" name="submit" class="btn btn-primary">Add Contact</button>
         </form>
 
