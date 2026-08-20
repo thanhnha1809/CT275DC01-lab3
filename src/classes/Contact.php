c35222557919ca38d0808b0b7b4ccec9621e9c8f
Huynh Thanh Nha <huynhthanhnhagt123123@gmail.com>
2026-08-20 19:02:05 +0700

    Update Contact.php

--- src/classes/Contact.php
+++ src/classes/Contact.php
@@ -94,4 +94,44 @@ protected function fillFromDbRow(array $row): Contact
 
         return $contacts;
     }
+    public function save(): bool
+    {
+        $result = false;
+
+        if ($this->id >= 0) {
+            $statement = $this->db->prepare(
+                'update contacts set name = :name,
+                    phone = :phone, notes = :notes, updated_at = now()
+                 where id = :id'
+            );
+            $result = $statement->execute([
+                'name' => $this->name,
+                'phone' => $this->phone,
+                'notes' => $this->notes,
+                'id' => $this->id
+            ]);
+        } else {
+            $statement = $this->db->prepare(
+                'insert into contacts (name, phone, notes, created_at, updated_at)
+                 values (:name, :phone, :notes, now(), now())'
+            );
+            $result = $statement->execute([
+                'name' => $this->name,
+                'phone' => $this->phone,
+                'notes' => $this->notes
+            ]);
+            if ($result) {
+                $this->id = $this->db->lastInsertId();
+            }
+        }
+
+        return $result;
+    }
+      public function fill(array $data): Contact
+    {
+        $this->name = trim($data['name'] ?? '');
+        $this->phone = trim($data['phone'] ?? '');
+        $this->notes = trim($data['notes'] ?? '');
+        return $this;
+    }
 }
