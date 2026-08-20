ac0243dfdc7456775ddd2548319c3c324abdd088
Huynh Thanh Nha <huynhthanhnhagt123123@gmail.com>
2026-08-20 18:39:28 +0700

    Update bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once 'functions.php';
+require_once __DIR__ . '/../libraries/Psr4AutoloaderClass.php';
+
+$loader = new Psr4AutoloaderClass;
+$loader->register();
+
+// Các lớp có không gian tên bắt đầu với CT275\Labs nằm ở src/classes
+$loader->addNamespace('CT275\Labs', __DIR__ . '/classes');
+
+try {
+    $PDO = (new CT275\Labs\PDOFactory())->create([
+        'dbhost' => 'localhost',
+        'dbname' => 'ct275_lab3',
+        'dbuser' => 'postgres',
+        'dbpass' => 'admin' 
+    ]);
+} catch (Exception $ex) {
+    echo 'Không thể kết nối đến PostgreSQL, kiểm tra lại username/password đến PostgreSQL.<br>';
+    exit("<pre>{$ex}</pre>");
+}
